800a395094aa1cff41a62c89178a7bbf226d968c
tik-tak-ololo <tik-tak-ololo@outlook.com>
2026-01-31 00:39:31 +0700

    Initial commit.

---
+++ ImageFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Сергей Хмелёв on 31.01.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
