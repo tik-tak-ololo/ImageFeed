934ccb42d0abeb4a038f8e7294e27f110245a4b2
tik-tak-ololo <tik-tak-ololo@outlook.com>
2026-02-07 13:49:03 +0700

    Added ProfileViewController

---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Сергей Хмелёв on 07.02.2026.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    @IBOutlet private var avatarImageView: UIImageView!
+    @IBOutlet private var nameLabel: UILabel!
+    @IBOutlet private var loginLabel: UILabel!
+    @IBOutlet private var descriptionLabel: UILabel!
+    @IBOutlet private var logoutButton: UIButton!
+    
+    
+    @IBAction func didTapLogoutButton(_ sender: Any) {
+    }
+    
+    
+}
