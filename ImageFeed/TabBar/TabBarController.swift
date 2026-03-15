0f9a81f5eb992f857e58dc57b8a45e314f8d14f3
tik-tak-ololo <tik-tak-ololo@outlook.com>
2026-03-16 01:58:04 +0700

    Added TabBarController

---
+++ ImageFeed/TabBar/TabBarController.swift
@@ -0,0 +1,30 @@
+//
+//  TabBarController.swift
+//  ImageFeed
+//
+//  Created by Сергей Хмелёв on 16.03.2026.
+//
+
+import UIKit
+ 
+final class TabBarController: UITabBarController {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        let storyboard = UIStoryboard(name: "Main", bundle: .main)
+             
+        let imagesListViewController = storyboard.instantiateViewController(
+            withIdentifier: "ImagesListViewController"
+        )
+             
+        let profileViewController = ProfileViewController()
+        profileViewController.tabBarItem = UITabBarItem(
+            title: "", // если подпись не нужна, оставьте пустую строку
+            image: UIImage(named: "tab_profile_active"),
+            selectedImage: nil
+        )
+            
+        self.viewControllers = [imagesListViewController, profileViewController]
+    }
+
+}
