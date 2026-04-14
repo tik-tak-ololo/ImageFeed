f3d40ac0c72e4e674021154427daed7bb9b11193
tik-tak-ololo <tik-tak-ololo@outlook.com>
2026-04-14 23:12:40 +0700

    refactor: WebViewPresenter

--- ImageFeed/Auth/WebViewPresenter.swift
+++ ImageFeed/Auth/WebViewPresenter.swift
@@ -17,23 +17,6 @@ final class WebViewPresenter: WebViewPresenterProtocol {
     }
     
     func viewDidLoad() {
-//        guard var urlComponents = URLComponents(string: Constants.unsplashAuthorizeURLString) else {
-//            return
-//        }
-//        
-//        urlComponents.queryItems = [
-//            URLQueryItem(name: "client_id", value: Constants.accessKey),
-//            URLQueryItem(name: "redirect_uri", value: Constants.redirectURI),
-//            URLQueryItem(name: "response_type", value: "code"),
-//            URLQueryItem(name: "scope", value: Constants.accessScope)
-//        ]
-//        
-//        guard let url = urlComponents.url else {
-//            return
-//        }
-//        
-//        let request = URLRequest(url: url)
-        
         didUpdateProgressValue(0)
         guard let request = authHelper.authRequest() else { return }
         view?.load(request: request)
